35575ad4d784b2883adae16a452680b7ffb54a93
Gyan Prakash <studyoneminute@gmail.com>
2021-08-06 13:05:43 +0530

    Create ternaryexpression.cpp

---
+++ Competitive Progamming/InterviewBit/Progamming Languages/C++/ternaryexpression.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int x, y;
+    cin>>x>>y;
+    
+    // YOUR CODE GOES HERE
+    cout << (x <= y ? "Robin" : "Rahul");
+    return 0;
+}
